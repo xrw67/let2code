88de3e4feee87930296f272d035363ff5719228e
xrw67 <xrw67@outlook.com>
2023-07-22 16:25:07 +0800

    fix vs compile error

--- src/005-longest-palindromic-substring.cpp
+++ src/005-longest-palindromic-substring.cpp
@@ -17,6 +17,7 @@ bool IsPalindrome(const char *s, size_t len) {
 
 // 给你一个字符串 s，找到 s 中最长的回文子串。
 // 如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
+//
 string longestPalindrome(string s) {
   if (s.length() == 1)
     return s;
